463dcb2bbf69ab31c9a938bc3f07d4880f437305
Mugdha Tiwari <mugdha.divya@gmail.com>
2026-02-10 20:43:44 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+
+    // Asking the user to give input
+    cout << "Enter three integers: ";
+    cin >> x >> y >> z;
+
+    // Printing the sum using arithmetic (+) operator
+    cout << "Sum = " << x + y + z << endl;
+
+    return 0;
+}
