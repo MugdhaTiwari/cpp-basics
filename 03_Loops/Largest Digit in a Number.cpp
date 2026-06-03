2d58b94a87dc03560c5f80f8193a4f4e28327f76
Mugdha Tiwari <mugdha.divya@gmail.com>
2026-06-03 21:07:38 +0530

    Largest digit in a number

--- 03_Loops/Largest Digit in a Number.cpp
+++ 03_Loops/Largest Digit in a Number.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n, number, largestDigit;
-    
+    int number, largestDigit = 0;
+    cout << "Enter a number: ";
+    cin  >> number; 
+
+    // Loop through each digit of the number
+    while ( number > 0 )
+    {
+        int digit = number % 10;         // Get the last digit
+        if ( digit > largestDigit )
+         {
+            largestDigit = digit;        // Update largestDigit if current digit is larger
+         }
+         
+         number =  number / 10;          // Remove the last digit
+    }
+    cout << "The largest digit in the number is: " << largestDigit << endl;
+     
+    return 0;
 }
