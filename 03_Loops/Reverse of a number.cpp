e4a15879a7f77add28525363a95b6e2406f6f754
Mugdha Tiwari <mugdha.divya@gmail.com>
2026-06-12 21:55:11 +0530

    Reverse a Number

---
+++ 03_Loops/Reverse of a number.cpp
@@ -0,0 +1,20 @@
+//Reverse a number
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number, rev_number = 0 ;
+    cout<<"Enter a number: ";
+    cin>>number;
+
+    while (number != 0)
+    {
+        rev_number = number % 10 + rev_number*10 ;
+        number = number / 10;
+    }
+
+    cout<< rev_number;
+
+    return 0;
+}
